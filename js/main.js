cdf6aa165ef9ad4f9bca912f6378c8f24e206bd0
sWitched-0ff <azhuravlov@gmail.com>
2014-03-24 22:53:47 +0400

    +top slider and fix a:hover style

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 	$('#partners').jcarousel({
 		wrap: 'circular'
 	});
+	$('#promoslider').jcarousel({
+		wrap: 'circular'
+	});
 	$('#prevslide').click(function() {
 		$('#partners').jcarousel('scroll', '-=1');
 		return false;
@@ -10,4 +13,31 @@
 		$('#partners').jcarousel('scroll', '+=1');
 		return false;
 	});
+	
+	var slideNav = $('#slidernavlist');
+	var slideList = $('#promoslider > ul');
+	
+	$('.slidernavlink').click(function(e){
+		e.preventDefault();
+		var slideNum = $(this).parent().index();
+		changeslide(slideNum,slideNav,slideList);
+		});
+	slideNav.find('li:first').find('a').trigger('click');
+	var timerId = setInterval(nextSlide, 10000,slideNav);
 });
+
+function changeslide(slideNum,slideNav,slideList){
+	slideNav.find('a').removeClass('active');
+	slideNav.find('li').eq(slideNum).find('a').addClass('active');
+	slideList.find('li').eq(slideNum).fadeIn();
+	slideList.find('li').not(slideList.find('li').eq(slideNum)).fadeOut(0);
+	};
+	
+function nextSlide(slideNav){
+	var nextNum = slideNav.find('.active').parent().index()+1;
+	var maxNum = slideNav.find('li').size();
+	if(nextNum >= maxNum){
+		nextNum = 0;
+		}
+	slideNav.find('a').eq(nextNum).trigger('click');
+	}
