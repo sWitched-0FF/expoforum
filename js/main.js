9e94443c2b0fdf578c12cba9170c38074e154e5f
sWitched-0ff <azhuravlov@gmail.com>
2014-03-24 17:42:22 +0400

    adaptive and js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function(){
+	$('#partners').jcarousel({
+		wrap: 'circular'
+	});
+	$('#prevslide').click(function() {
+		$('#partners').jcarousel('scroll', '-=1');
+		return false;
+	});
+	$('#nextslide').click(function() {
+		$('#partners').jcarousel('scroll', '+=1');
+		return false;
+	});
+});
